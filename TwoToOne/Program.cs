b1d90a7399edcabaa6061748df04aefe97d1fd0a
OldBeliver <380506338004@yandex.ua>
2022-04-01 13:28:58 +0300

    Добавьте файлы проекта.

---
+++ TwoToOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoToOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArray = new int[] { 1, 3, 1 };
+            int[] secondArray = new int[] { 1, 2, 2 };
+
+            List<int> tempoArray = new List<int>();
+            List<int> onlyOne = new List<int>();
+
+            tempoArray.AddRange(firstArray);
+            tempoArray.AddRange(secondArray);
+
+            for (int i = 0; i < tempoArray.Count; i++)
+            {
+                bool isContains = onlyOne.Contains(tempoArray[i]);
+
+                if (!isContains)
+                    onlyOne.Add(tempoArray[i]);
+            }
+
+            Console.WriteLine($"имеются два массива чисел:\n{string.Join(", ", firstArray)}\n{string.Join(", ", secondArray)}");
+            Console.WriteLine($"\nобъединенная коллекция с уникальными значениями:\n{string.Join(", ", onlyOne)}");
+        }
+    }
+}
