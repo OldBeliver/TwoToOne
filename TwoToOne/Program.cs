0ef78c9bc498edd8b1fa9a4aa70a220db29eaccb
OldBeliver <380506338004@yandex.ua>
2022-04-01 14:07:39 +0300

    косметические исправления

--- TwoToOne/Program.cs
+++ TwoToOne/Program.cs
@@ -10,23 +10,25 @@ namespace TwoToOne
         {
             int[] firstArray = new int[] { 1, 3, 1 };
             int[] secondArray = new int[] { 1, 2, 2 };
-
-            List<int> tempoArray = new List<int>();
+            
             List<int> onlyOne = new List<int>();
 
-            tempoArray.AddRange(firstArray);
-            tempoArray.AddRange(secondArray);
-
-            for (int i = 0; i < tempoArray.Count; i++)
-            {
-                bool isContains = onlyOne.Contains(tempoArray[i]);
-
-                if (!isContains)
-                    onlyOne.Add(tempoArray[i]);
-            }
+            GetUniqueValues(firstArray, ref onlyOne);
+            GetUniqueValues(secondArray, ref onlyOne);
 
             Console.WriteLine($"имеются два массива чисел:\n{string.Join(", ", firstArray)}\n{string.Join(", ", secondArray)}");
             Console.WriteLine($"\nобъединенная коллекция с уникальными значениями:\n{string.Join(", ", onlyOne)}");
+        }
+
+        static void GetUniqueValues(int[] array, ref List<int> onlyOne)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int value = array[i];
+
+                if (!onlyOne.Contains(value))
+                    onlyOne.Add(value);
+            }
         }
     }
 }
